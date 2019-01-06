5053896e2e349641608b0d8de70ac6e5081b2bf0
Matěj Račinský <racinsky.matej@seznam.cz>
2019-01-06 22:08:40 +0100

    added merging ender items, types collision fixed

--- inventory-handling/merge_players_data.py
+++ inventory-handling/merge_players_data.py
@@ -27,10 +27,13 @@ def main(_):
     usercache_file_new = r'E:\Projects\minecraft\new-mc-server\usercache.json'
     dir_out = '.'
 
-    old_corrupted_hashes = [    # empirically found
-        '6220dacd-98ba-3371-873c-baac80c0b989',
-        '985969ce-f9cc-38b0-a7ea-2a2097f0d513',
-    ]
+    forbidden_mod = 'twilightforest'
+
+    old_corrupted_hashes = []
+    # old_corrupted_hashes = [    # empirically found
+    #     '6220dacd-98ba-3371-873c-baac80c0b989',
+    #     '985969ce-f9cc-38b0-a7ea-2a2097f0d513',
+    # ]
 
     with open(usercache_file_old, 'rb') as fp:
         user_hashes_old = json.load(fp)
@@ -43,15 +46,15 @@ def main(_):
 
     playerdata_files_old = {f: osp.join(playerdata_dir_old, f) for f in os.listdir(playerdata_dir_old)}
     playerdata_files_new = {f: osp.join(playerdata_dir_new, f) for f in os.listdir(playerdata_dir_new)}
-    for filename, file_path in playerdata_files_new.items():
-        file_hash = osp.splitext(osp.basename(filename))[0]
-        nbtfile_new = nbt.NBTFile(file_path, 'rb')
+    for filename_new, file_path_new in playerdata_files_new.items():
+        file_hash_new = osp.splitext(osp.basename(filename_new))[0]
+        nbtfile_new = nbt.NBTFile(file_path_new, 'rb')
 
-        if file_hash not in uuid_to_name_old:
-            print('unknown hash', file_hash, 'skipping')
+        if file_hash_new not in uuid_to_name_new:
+            print('unknown hash', file_hash_new, 'skipping')
             continue
 
-        player_name = uuid_to_name_new[file_hash]
+        player_name = uuid_to_name_new[file_hash_new]
         if player_name not in name_to_uuid_old:
             print('player', player_name, 'not found in old usercache')
             continue
@@ -71,9 +74,41 @@ def main(_):
         old_forgecaps = nbtfile_old['ForgeCaps']
         old_forgecaps.update(nbtfile_new['ForgeCaps'])  # old data, merged with new, same parts overwritten by new
         nbtfile_new['ForgeCaps'] = old_forgecaps
-        nbtfile_new['Inventory'].extend(nbtfile_old['Inventory'])   # just appending data
 
-        nbtfile_new.write_file(osp.join(dir_out, filename))
+        all_slots = list(range(36))
+        all_ender_slots = list(range(27))
+        new_slots_full = [i['Slot'].value for i in nbtfile_new['Inventory']]
+        # new_ender_slots_full = [i['Slot'].value for i in nbtfile_new['EnderItems']]
+        new_ender_slots_full = []
+
+        # filtering out items from forbidden mod
+        for i, item in enumerate(nbtfile_old['Inventory']):
+            if item['id'].value.startswith(forbidden_mod):
+                del nbtfile_old['Inventory'][i]
+        for i, item in enumerate(nbtfile_old['EnderItems']):
+            if item['id'].value.startswith(forbidden_mod):
+                del nbtfile_old['EnderItems'][i]
+
+        old_slots_full = [i['Slot'].value for i in nbtfile_old['Inventory']]
+        old_ender_slots_full = [i['Slot'].value for i in nbtfile_old['EnderItems']]
+        # old equipped things will go to inventory
+        num_new_free_slots = len(list(set(all_slots) - set(new_slots_full))) + len(
+            list(set(all_ender_slots) - set(new_ender_slots_full)))
+        if len(old_slots_full) + len(old_ender_slots_full) > num_new_free_slots:
+            print('Too many items together, cant merge for player', player_name, ', should have', len(old_slots_full),
+                  'free slots for transfer')
+            continue
+        # todo: change Slot number so old items are set to free slots of new inventory
+        nbtfile_new['Inventory'].extend(nbtfile_old['Inventory'])  # just appending data
+        nbtfile_new['EnderItems'].extend(nbtfile_old['EnderItems'])  # just appending data
+        # when adding non-empty to empty list, types collapse
+        nbtfile_new['EnderItems'].tagID = nbtfile_old['EnderItems'].tagID
+        # todo: nějak se jebe enderchest merging
+        for i in nbtfile_old['Inventory']:
+            if i['id'].value.startswith('twilight'):
+                break
+
+        nbtfile_new.write_file(osp.join(dir_out, filename_new))
 
 
 if __name__ == '__main__':
